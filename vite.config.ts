894a70220240eb871fd57cd20b354f9af96192a6
Hetal Joshi <johihetal54@gmail.com>
2025-03-30 07:20:44 -0600

    2nd

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/", // Use "/" if deployed at domain root. Otherwise, set to your subdirectory (e.g., "/my-app/")
   resolve: {
     alias: {
       // This alias tells Vite that any import of "bootstrap-esm"
